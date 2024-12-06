504267b66da05beff8202714425199981571c2b5
JimenaAgustinNavarro <jimenaagustin0@gmail.com>
2024-12-06 09:37:23 -0500

    estilos, menu, y nuevos archivos en pages

--- e-commerce/src/components/Navbar/Navbar.jsx
+++ e-commerce/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import cart_icon from '../../assets/cart_icon.png'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -11,14 +14,15 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className='nav-menu'>
-            <li>Shop <hr/></li>
-            <li>Hombre <hr/></li>
-            <li>Mujer <hr/></li>
-            <li>Niños <hr/></li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}>Hombre{menu==="mens"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}>Mujer{menu==="womens"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Niños{menu==="kids"?<h/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+            <div className="nav-cart-count"></div>
         </div>
     </div>
   )
